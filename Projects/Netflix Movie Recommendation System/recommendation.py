d74fb0ded950f6d6825b9d5ce7a5f39622ccf2d2
being-iota <ratnapriya2954@gmail.com>
2025-03-16 23:39:20 +0530

    updated

---
+++ Projects/Netflix Movie Recommendation System/recommendation.py
@@ -0,0 +1,9 @@
+import pandas as pd 
+
+ratings = pd.read_csv("ml-100k/u.data", sep="\t", names=["user_id", "movie_id", "rating", "timestamp"])
+
+movies = pd.read_csv("ml-100k/u.item", sep="|", encoding="latin-1", usecols=[0, 1], names=["movie_id", "title"])
+
+movie_ratings = pd.merge(ratings, movies, on="movie_id")
+
+print(movie_ratings.head())
