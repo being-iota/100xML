3f05aeb165b958b86e6371fc00409b38ddbfbad4
being-iota <ratnapriya2954@gmail.com>
2025-03-17 23:16:55 +0530

    updated

--- Projects/Netflix Movie Recommendation System/recommendation.py
+++ Projects/Netflix Movie Recommendation System/recommendation.py
@@ -4,6 +4,11 @@ ratings = pd.read_csv("ml-100k/u.data", sep="\t", names=["user_id", "movie_id", 
 
 movies = pd.read_csv("ml-100k/u.item", sep="|", encoding="latin-1", usecols=[0, 1], names=["movie_id", "title"])
 
+
 movie_ratings = pd.merge(ratings, movies, on="movie_id")
+
+movie_ratings.drop(columns=["timestamp"], inplace=True)
+
+print("Missing Values:\n", movie_ratings.isnull().sum())
 
 print(movie_ratings.head())
